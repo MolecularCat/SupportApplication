1c3b31d96d60d585c3562c9db4ac95f89fbccba4
MolecularCat <geb@inbox.ru>
2023-02-19 15:44:53 +0300

    add project

---
+++ SupportApplication/Models/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SupportApplication.Models
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<Messages> Messages { get; set; }
+    }
+}
---
+++ SupportApplication/Repositories/MessagesRepository.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.Configuration;
+using SupportApplication.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SupportApplication.Repositories
+{
+    public class MessagesRepository
+    {
+        private readonly IConfiguration _configration;
+        private readonly AppDbContext context;
+
+        public MessagesRepository(IConfiguration configuration, AppDbContext context)
+        {
+            _configration = configuration;
+            this.context = context;
+        }
+
+        public List<Messages> GetAllMessages()
+        {
+            var messages = context.Messages.ToList();
+            return messages;
+        }
+    }
+}
